498287677c64c21419adf5846b951bb5fff6fee7
nivedita0604 <niveditakumari0604@gmail.com>
2021-06-29 20:59:26 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 const App = () => {
   return (
     <Switch>
@@ -12,6 +13,9 @@ const App = () => {
       <Route exact path="/starred">
         {' '}
         <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
       <Route>
         <div>This is 404 Page</div>
