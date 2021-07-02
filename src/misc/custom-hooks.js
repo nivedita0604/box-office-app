634a9ca300ec09f0c49bc3ff5bbfef141ee0373e
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-02 20:45:38 +0530

    few optimizations

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useReducer, useEffect, useState } from 'react';
+import { useReducer, useEffect, useState, useRef, useCallback } from 'react';
 import { apiGet } from '../misc/config';
 function showsReducer(prevState, action) {
   switch (action.type) {
@@ -32,15 +32,19 @@ function usePersistedReducer(reducer, initialState, key) {
 export function useShows(key = 'shows') {
   return usePersistedReducer(showsReducer, [], key);
 }
+
 export function useLastQuery(key = 'lastQuery') {
   const [input, setInput] = useState(() => {
     const persisted = sessionStorage.getItem(key);
     return persisted ? JSON.parse(persisted) : '';
   });
-  const setPersistedInput = newState => {
-    setInput(newState);
-    sessionStorage.setItem(key, JSON.stringify(newState));
-  };
+  const setPersistedInput = useCallback(
+    newState => {
+      setInput(newState);
+      sessionStorage.setItem(key, JSON.stringify(newState));
+    },
+    [key]
+  );
   return [input, setPersistedInput];
 }
 
@@ -86,4 +90,35 @@ export function useShow(showId) {
     };
   }, [showId]);
   return state;
+}
+
+export function useWhyDidYouUpdate(name, props) {
+  // Get a mutable ref object where we can store props ...
+  // ... for comparison next time this hook runs.
+  const previousProps = useRef();
+  useEffect(() => {
+    if (previousProps.current) {
+      // Get all keys from previous and current props
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      // Use this object to keep track of changed props
+      const changesObj = {};
+      // Iterate through keys
+      allKeys.forEach(key => {
+        // If previous is different from current
+        if (previousProps.current[key] !== props[key]) {
+          // Add to changesObj
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+      // If changesObj not empty then output to console
+      if (Object.keys(changesObj).length) {
+        console.log('[why-did-you-update]', name, changesObj);
+      }
+    }
+    // Finally update previousProps with current props for next hook call
+    previousProps.current = props;
+  });
 }
