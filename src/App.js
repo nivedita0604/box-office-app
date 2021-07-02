e191df4303ac9209e1eb2c925d7a6ef5fd1e4586
nivedita0604 <niveditakumari0604@gmail.com>
2021-07-02 19:37:04 +0530

    styled comp for nav and title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { ThemeProvider } from 'styled-components';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        {' '}
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        {' '}
-        <Starred />
-      </Route>
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
-      <Route>
-        <div>This is 404 Page</div>
-      </Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          {' '}
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          {' '}
+          <Starred />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+        <Route>
+          <div>This is 404 Page</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 };
 
